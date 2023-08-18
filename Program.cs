54da7bfeafbb9182cab1f525a8b535c44dd74570
Шараев С.А <ssa@iro.ru>
2023-08-18 14:10:52 +0700

    Ex15

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+
+if (a<0) 
+{
+    Console.WriteLine("Номер не день недели");
+    return;
+}
+else if (a>7)
+{
+    Console.WriteLine("Номер не день недели");
+    return;
+}
+else
+{
+    if(a==6)
+    {
+        Console.WriteLine("Да");
+        return;
+    }
+    else if(a==7)
+    {
+        Console.WriteLine("Да");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+        return;
+    }
+}
