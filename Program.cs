2fa4a18b2fcee0a2e62cda6e3dad7c9d874c6833
Шараев С.А <ssa@iro.ru>
2023-08-18 13:58:55 +0700

    Ex13

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+
+if (a<100)
+{
+    Console.WriteLine("Третьего числа нет");
+    return;
+}
+else if (a>999)
+{
+    while(a>999)
+    {
+        a = a / 10;
+    }
+}
+
+a = a % 10;
+Console.WriteLine(a);
