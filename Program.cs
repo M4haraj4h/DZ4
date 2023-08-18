6ff691941a4857f3e7449ac730098c9b8816894b
Шараев С.А <ssa@iro.ru>
2023-08-18 13:33:44 +0700

    Ex2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(a.ToString().Length);
+
+if (b<3)
+{
+    Console.WriteLine("Число меньше чем трёхзначное");
+    return;
+}
+else if (b>3)
+{
+    Console.WriteLine("Число больше чем трёхзначное");
+    return;
+}
+else if (b==3)
+{
+    a = a/10;
+    a = a % 10;
+    Console.WriteLine(a);
+}
